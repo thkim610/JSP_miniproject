710d7d3a75e630ac1b30113946d520eb58121986
thkim610 <thkim610@naver.com>
2023-09-17 17:44:24 +0900

    feat: userDTO 구현

---
+++ BBSProject/src/main/java/user/UserDTO.java
@@ -0,0 +1,42 @@
+package user;
+
+public class UserDTO {
+	
+	private String userID;
+	private String userPWD;
+	private String userName;
+	private String userGender;
+	private String userEmail;
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getUserPWD() {
+		return userPWD;
+	}
+	public void setUserPWD(String userPWD) {
+		this.userPWD = userPWD;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserGender() {
+		return userGender;
+	}
+	public void setUserGender(String userGender) {
+		this.userGender = userGender;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	
+	
+}
